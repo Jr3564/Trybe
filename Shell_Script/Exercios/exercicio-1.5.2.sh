e0962d3217abba2f2045fdd4e42cc89d192c55eb
Rodolfo <rodolfojr94@gmail.com>
2020-08-19 21:29:24 -0300

    Feito exercicio 2

---
+++ Shell_Script/Exercios/exercicio-1.5.2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#2. Modifique o Shell Script do exercício anterior para incluir uma variável. 
+#A variável deverá receber o conteúdo da mensagem: "Shell Script com variáveis é demais!";
+
+    VAR=" Shell Script com variáveis é demais! "
+        
+        echo $VAR
