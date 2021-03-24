1ded2472498b98cb4800449b2f40b1386c723e03
Jr3564 <rodolfojr94@gmail.com>
2021-03-24 11:31:49 -0300

    Exercícios prontos

--- Desenvolvimento-Back-End/exercises/introducao-ao-desenvolvimento-web-com-nodejs_1/calcula-imc.readline-sync.js
+++
@@ -1,9 +0,0 @@
-const { questionFloat } =  require('readline-sync') ; 
-const { imcMessage } = require('./IMC');
-
-const weight = questionFloat("Qual é seu peso?");
-const height = questionFloat("Qual é sua altura?");
-
-const message = imcMessage(weight, height);
-
-console.log(message);
